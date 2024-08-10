f5128f8965dd646b5177c3980108c43ed30925b8
Juan Carlos Dominguez <jcarlosdominguez38@gmail.com>
2024-08-09 22:07:28 -0700

    menu toggle

--- static/JavaScript/script.js
+++ static/JavaScript/script.js
@@ -12,3 +12,25 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }, 100); 
 });
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
+
+//     dropdownToggles.forEach(toggle => {
+//         toggle.addEventListener('click', function (event) {
+//             event.preventDefault(); 
+
+        
+//             const icon = toggle.querySelector('.fas.fa-caret-down');
+//             if (icon) {
+//                 icon.classList.toggle('rotate');
+//             }
+
+//             const submenu = toggle.nextElementSibling;
+//             if (submenu && submenu.classList.contains('sub-menu')) {
+//                 submenu.classList.toggle('open');
+//             }
+//         });
+//     });
+// });
+
